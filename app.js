da0ce187fa21ae39453c1d80502cc081cef86673
devahmedabdo <devahmedabdo@gmail.com>
2022-07-05 18:23:02 +0200

    first push

---
+++ app.js
@@ -0,0 +1,72 @@
+const yargs = require("yargs");
+const students = require("./students");
+
+yargs.command(
+  {
+  command:'add',
+  descripe:'add student details',
+  builder:{
+    id:{
+      descripe:"id of student"
+      , demandOption:true
+      , type:Number
+    },
+    name:{
+      descripe:"name of student"
+      , demandOption:true
+      , type:String
+    },
+    degrees:{
+      descripe:"degrees of student"
+      , demandOption:true
+      , type:String
+    },
+    comment:{
+      descripe:"comment on student"
+      , type:String
+    },
+  },
+  handler:()=>{
+    students.addStudent(yargs.argv.id,yargs.argv.name,yargs.argv.degrees,yargs.argv.comment)
+  }
+}
+)
+yargs.command(
+  {
+  command:'delete',
+  descripe:'delete student details',
+  builder:{
+    id:{
+      descripe:"id of student"
+      , demandOption:true
+      , type:Number
+    },},
+  handler:()=>{
+    students.deleteStudent(yargs.argv.id)
+  }
+})
+yargs.command(
+  {
+  command:'read',
+  descripe:'read student details',
+  builder:{
+    id:{
+      descripe:"id of student"
+      , demandOption:true
+      , type:Number
+    },},
+  handler:()=>{
+    students.readStudent(yargs.argv.id)
+  }
+})
+yargs.command(
+  {
+  command:'list',
+  descripe:'list of student details',
+ 
+  handler:()=>{
+    students.listStudents()
+  }
+})
+
+yargs.parse();
---
+++ students.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const addStudent = (id, name,degrees ,comment) => {
+  const students = loadStudentDetails();
+  const existStudent = students.find((ele) => {
+    return ele.id === id;
+  });
+  if (!existStudent) {
+    let studentDegrees = degrees.split(',')
+    let total = 0 
+    for(let degree of studentDegrees){
+      total += +degree
+    } 
+    students.push({
+      id,
+      name,
+      total,
+      degrees,
+
+      comment
+
+    });
+
+    saveData(students);
+    console.log("sucsessfully added one student");
+  } else {
+    console.log("this student already exsist");
+  }
+};
+
+const loadStudentDetails = () => {
+  try {
+    const details = fs.readFileSync("students.json").toString();
+    return JSON.parse(details);
+  } catch (e) {
+    return [];
+  }
+};
+const saveData = (students) => {
+  const saveData = JSON.stringify(students);
+
+  fs.writeFileSync("students.json", saveData);
+};
+// delete
+const deleteStudent = (id) => {
+  let data = loadStudentDetails();
+  let existStudent = data.filter((ele) => {
+    return ele.id !== id;
+  });
+  if (data.length !== existStudent.length) {
+    saveData(existStudent);
+    console.log("student deleted sucsessfully");
+  } else {
+    console.log("student is not found");
+  }
+};
+// read
+const readStudent = (id) => {
+  let data = loadStudentDetails();
+  let studentWanted = data.find((ele) => {
+    return ele.id === id;
+  });
+  if (studentWanted) {
+    console.log(`The Student Name Is => ${studentWanted.name} And his total degrees is => ${studentWanted.total}`);
+  } else {
+    console.log(`this id is not linked to any student`);
+  }
+};
+// list
+const listStudents = (title) => {
+  let data = loadStudentDetails();
+  let num = 1
+  data.forEach((ele) => {
+    
+    if (data!==0) {
+      console.log(`Student number ${num} is ${ele.name}`);
+      num++
+    } else {
+      console.log("no student added yet!");
+    }
+  });
+};
+
+module.exports = {
+  addStudent,
+  deleteStudent,
+  readStudent,
+  listStudents,
+};
